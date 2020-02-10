04b5b593ab215f971974f7fffedb8007319fd617
Mustafa GUNES <gunes149@gmail.com>
2020-02-10 11:28:55 +0300

    project created

---
+++ Reborn/Reborn.h
@@ -0,0 +1,19 @@
+//
+//  Reborn.h
+//  Reborn
+//
+//  Created by Mustafa GUNES on 10.02.2020.
+//  Copyright © 2020 Mustafa GUNES. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Reborn.
+FOUNDATION_EXPORT double RebornVersionNumber;
+
+//! Project version string for Reborn.
+FOUNDATION_EXPORT const unsigned char RebornVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reborn/PublicHeader.h>
+
+
