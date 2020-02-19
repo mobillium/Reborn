8e34973136ac30a299b8e7e3b535e8e6b6b158b2
tunay alver <tunay.alver@mobillium.com>
2020-02-19 10:59:36 +0300

    the extension is a var which return the name of the class.

---
+++ RebornTests/UIViewController/UIViewControllerTests.swift
@@ -0,0 +1,28 @@
+//
+//  UIViewControllerTests.swift
+//  RebornTests
+//
+//  Created by tunay alver on 18.02.2020.
+//  Copyright © 2020 Mustafa GUNES. All rights reserved.
+//
+
+import XCTest
+@testable import Reborn
+
+final class UIViewControllerTests: XCTestCase {
+
+    func testWithUIController() {
+        let identifier = UIViewController.identifier
+        XCTAssertEqual(identifier, "UIViewController")
+    }
+    
+    func testWithController() {
+        XCTAssertEqual(TestClass.identifier, "TestClass")
+    }
+
+}
+
+//MARK: - Test Class
+class TestClass: UIViewController {
+    
+}
