e558f2165bc045597ac0fc7a911cc5661b209689
Mehmet Eroğlu <mehmet---eroglu@hotmail.com>
2020-02-19 11:01:54 +0300

    Replacing a fix number of substring with a new string function created

---
+++ Sources/Foundation/StringExtensions.swift
@@ -0,0 +1,37 @@
+//
+//  StringExtensions.swift
+//  Reborn
+//
+//  Created by Mehmet Eroğlu on 18.02.2020.
+//  Copyright © 2020 Mustafa GUNES. All rights reserved.
+//
+
+#if canImport(Foundation)
+import Foundation
+
+public extension String {
+    
+    /// Replacing a fix number of substrings with another substring.
+    ///
+    /// - Parameters:
+    ///   - search: Search substring for replacement.
+    ///   - replacement: Substring that we want to replace with.
+    ///   - maxReplacements: Fix number of substring that we want to replace.
+    /// - Returns: Give the new string with all replacements (if exists).
+    func replacingOccurrences(of search: String, with replacement: String, count maxReplacements: Int) -> String {
+        var count = 0
+        var returnValue = self
+
+        while let range = returnValue.range(of: search) {
+            returnValue = returnValue.replacingCharacters(in: range, with: replacement)
+            count += 1
+            
+            // exit as soon as we've made all replacements
+            if count == maxReplacements {
+                return returnValue
+            }
+        }
+        return returnValue
+    }
+}
+#endif
